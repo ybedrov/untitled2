e130e9e4acc4015c0b98cd2d0218cdf5c23291de
jetbrains <jet@jet.ru>
2020-03-10 12:54:43 +0300

    gg

--- src/Main.java
+++ src/Main.java
@@ -1,95 +1,43 @@
+/*
+ * Copyright (c) 2020 CoreLogic, Inc. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of CoreLogic, Inc.
+ * It is furnished under license and may only be used or copied in accordance
+ * with the terms of such license.
+ * This software is subject to change without notice and no information
+ * contained in it should be construed as commitment by CoreLogic, Inc.
+ * CoreLogic, Inc. cannot accept any responsibility, financial or otherwise, for any
+ * consequences arising from the use of this software except as otherwise stated in
+ * the terms of the license.
+ */
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-
-        one onne = new one();
-
-        onne.test();
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("hrrf");
 
 
-        //use one.test()
+        String values = null;
+        //  values.split("(?<=\\G.{" + getMaxCharAmount() + "})");
 
-        onne.test();
+
+        try {
+            System.out.println(InetAddress.getLocalHost()
+                                       .getCanonicalHostName());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println("taskA");
+
+
+    }
+
+    private static String getMaxCharAmount() {
+
+        return "test";
     }
 }
